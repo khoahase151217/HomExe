d0052ca4d346f3d7f4871f22484445a4355ad626
zhangliejunjie <kiettltse161289@fpt.edu.vn>
2022-10-15 15:33:44 +0700

    help

--- src/layouts/dashboard/NavConfig.js
+++ src/layouts/dashboard/NavConfig.js
@@ -7,7 +7,7 @@ const getIcon = (name) => <Iconify icon={name} width={22} height={22} />;
 
 const navConfig = [
   {
-    title: 'dashboard',
+    title: 'My Personal Training',
     path: '/dashboard/app',
     icon: getIcon('eva:pie-chart-2-fill'),
   },
